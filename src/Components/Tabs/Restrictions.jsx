46ab7e085f65b2492d22395a86e36b9bec7826bc
Jacob Sullivan <jay@jaysull.dev>
2018-08-01 14:33:23 -0600

    Adding the restrictions I think Jess wants on here

--- src/Components/Tabs/Restrictions.jsx
+++ src/Components/Tabs/Restrictions.jsx
@@ -7,14 +7,37 @@ class Restrictions extends React.Component {
         <h2 className="tab-heading">
           Horizon Hills Community Rules and Restrictions
         </h2>
-        <p>
-          Phasellus sodales ullamcorper eros eu sagittis. Donec sed semper enim.
-          Cras non mi tempor, maximus elit in, facilisis erat. In suscipit nulla
-          ac eros sagittis mattis. Fusce dignissim tristique erat, at
-          consectetur magna tempor ac. Nullam et risus sit amet sem commodo
-          viverra ac sagittis elit. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos.
-        </p>
+        <hr />
+        <ul>
+          <li>
+            At least one resident for each home within Sunflower shall be age 55
+            or better.
+          </li>
+          <li>
+            No resident or guest may create loud or disturbing noises between
+            the hours of 9:00 p.m. and 7:00 a.m.
+          </li>
+          <li>
+            The maximum number of domestic household pets is two (2); any
+            combination is acceptable (i.e.,:2 dogs or 2 cats or 1 of each). No
+            animals may cause a disturbance.
+          </li>
+          <li>
+            The exterior of each home in the community shall be maintained in a
+            neat, clean condition and remain in good physical repair.
+          </li>
+          <li>
+            Sunflower facilities are provided for the exclusive use of the
+            residents and their guests.
+          </li>
+          <li>
+            Home-based businesses are permitted, but must seek management's
+            written approval, and are subject to the Sunflower Rules and
+            Regulations. Automotive repair businesses are prohibited.
+          </li>
+          <li>Small satellite dishes are permitted.</li>
+          <li>NO RENTERS ALLOWED - Owner Occupied Only.</li>
+        </ul>
       </section>
     );
   }

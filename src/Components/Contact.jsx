8c015b0d87a4f0eab4c20fdee75e92f41667c8ff
Jacob Sullivan <jay@jaysull.dev>
2018-08-04 21:33:09 -0600

    Styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -33,7 +33,7 @@ class Contact extends React.Component {
               </div>
             </div>
             <div className="card-content">
-              <div className="columns">
+              <div className="columns contact-card-columns">
                 <div className="column content">
                   <h1>Hello</h1>
                   <p>

143e109a6ec14c0424faebc38c9966d8523494e0
Jacob Sullivan <jay@jaysull.dev>
2022-07-22 07:40:55 -0700

    Remove log; Re-add service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// import { register } from "./registerServiceWorker";
+import { register } from "./registerServiceWorker";
 import "bulma/css/bulma.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
-console.log(root);
-// register();
+register();
